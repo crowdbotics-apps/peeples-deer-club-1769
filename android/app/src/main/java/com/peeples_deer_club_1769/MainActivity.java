9ef800d549f72aa325791e7b5f306f111c648d0e
Crowdbotics <team@crowdbotics.com>
2019-03-29 22:45:58 +0000

    initial commit

---
+++ android/app/src/main/java/com/peeples_deer_club_1769/MainActivity.java
@@ -0,0 +1,15 @@
+package com.peeples_deer_club_1769;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "peeples_deer_club_1769";
+    }
+}
